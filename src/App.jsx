b216e7c52ec2a05ae1c0d0ca82e102c6f11e887e
Salai Zaw Htet Aung <hzaw9036@gmail.com>
2024-01-11 05:10:26 -0800

    inputForm added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import InputForm from './components/InputForm'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <InputForm/>
+    </div>
   )
 }
 
